3baae9b1a3014bb78084ab3be5dc2e90c7406b96
hsotelodlt22-cloud <hsotelodlt22@gmail.com>
2026-02-24 17:53:15 -0500

    Update layout.tsx

--- App/src/app/layout.tsx
+++ App/src/app/layout.tsx
@@ -1,25 +1,18 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import { AuthProvider } from "../contexts/AuthContext";
 import { AccountProvider } from "../contexts/AccountContext";
 
-export default function RootLayout({
+export default function Providers({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="es">
-      <body>
-        <SessionProvider>
-          <AuthProvider>
-            <AccountProvider>
-              {children}
-            </AccountProvider>
-          </AuthProvider>
-        </SessionProvider>
-      </body>
-    </html>
+    <SessionProvider>
+      <AccountProvider>
+        {children}
+      </AccountProvider>
+    </SessionProvider>
   );
 }
