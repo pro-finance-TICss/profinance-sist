c97177a658699ceb2c13ddc8d674d5abb9e407c5
hsotelodlt22-cloud <hsotelodlt22@gmail.com>
2026-02-25 18:18:35 -0500

    Update providers.tsx

--- App/src/app/providers.tsx
+++ App/src/app/providers.tsx
@@ -1,18 +1,9 @@
 "use client";
 
-import { SessionProvider } from "next-auth/react";
-import { AccountProvider } from "../contexts/AccountContext";
-
 export default function Providers({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <SessionProvider>
-      <AccountProvider>
-        {children}
-      </AccountProvider>
-    </SessionProvider>
-  );
+  return <>{children}</>;
 }
